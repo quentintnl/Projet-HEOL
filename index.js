7dea04720007766ff7b64e705bd5087334ef6bf2
Kuro <45374906+kuro250104@users.noreply.github.com>
2022-12-15 15:14:05 +0100

    add select in product1.html

---
+++ index.js
@@ -0,0 +1,8 @@
+const counter = document.getElementById("count");
+const add_one = document.getElementById("add_one")
+const delete_one = document.getElementById("delete_one")
+
+counter.value=1
+onclick(counter.value+1)
+counter.value=1+4;
+counter.innerHTML = counter.value;
